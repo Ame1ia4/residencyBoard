596441e0bad646f9525e04f4a4332de1b5279270
Caitlin Amelia <ameliamolon@gmail.com>
2025-05-26 10:51:40 +0100

    additions

---
+++ jobsBoard/src/components/dropdown1.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+function DropdownMenu (){
+  return (
+    <div className="dropdown-menu">
+      <ul>
+        <li>
+        <a href="/r1">R1</a>
+        </li>
+        <li>
+        <a href="/r1-2">R1+2</a>
+        </li>
+        <li>
+        <a href="/r1">R2</a>
+        </li>
+        <li>
+        <a href="/r1">R3</a>
+        </li>
+        <li>
+        <a href="/r1">R4</a>
+        </li>
+        <li>
+        <a href="/r1">R5</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default DropdownMenu;
--- jobsBoard/src/components/navbar.jsx
+++ jobsBoard/src/components/navbar.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import DropdownMenu from "./dropdown1";
 
 export default function Navbar() {
+
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const handleMouseEnter = () => {
+    setDropdownVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(false);
+  };
+
   return (
     <nav className="nav">
      <ul>
@@ -8,7 +21,15 @@ export default function Navbar() {
           <a href="/">Home</a>
       </li>
       <li>
+      <div
+          className="menu"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
           <a href="/details">Job Details</a>
+           {/* <DropdownMenu /> */}
+           {isDropdownVisible && <DropdownMenu />}
+        </div>
       </li>
       <li>
         <a href="/ranking">Ranking</a>
