350fb905070c2481ac189c8247cde9515f8d6308
CiaranLy <lynchbros3@gmail.com>
2025-05-26 12:31:36 +0100

    sign up fields

--- jobsBoard/src/components/loginForm.jsx
+++ jobsBoard/src/components/loginForm.jsx
@@ -5,9 +5,14 @@ export default function LoginForm(){
     // variables for input values initialised to empty strings
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    // variable for Login / SignUp initalised true for Login
-    const [isLogin, setIsLogin] = useState(true);
 
+    const [fname, setFname] = useState('');
+    const [lname, setLname] = useState('');
+    const [cname, setCname] = useState('');
+    const [role, setRole] = useState('');
+    const [studentId, setSID] = useState('');
+
+    const [isLogin, setIsLogin] = useState(null);
     // called by form submit
     const handleSubmit = async (e) => {
         e.preventDefault(); // prevents page from auto reloading and form from auto submitting
@@ -24,22 +29,111 @@ export default function LoginForm(){
     };
 
     return(
-        <form onSubmit={handleSubmit} className="form">
-            <input className="input1"
+        <form onSubmit={handleSubmit}>
+            {!isLogin && (
+                <>
+                    <p>Please select your Role.</p>
+
+                    <label>
+                        <input
+                            type="radio"
+                            name="role"
+                            value='student'
+                            checked={role === 'student'}
+                            onChange={(e) => setRole(e.target.value)}
+                        />
+                        Student
+                        <input
+                            type="radio"
+                            name="role"
+                            value='staff'
+                            checked={role === 'staff'}
+                            onChange={(e) => setRole(e.target.value)}
+                        />
+                        Staff
+                        <input
+                            type="radio"
+                            name="role"
+                            value='residency'
+                            checked={role === 'residency'}
+                            onChange={(e) => setRole(e.target.value)}
+                        />
+                        Residency Partner
+                    </label>
+                </>
+            )}
+            <br></br>
+            <br></br>
+            
+            <input
                 type="email"
                 placeholder="Email"
                 value={email}
                 onChange={e => setEmail(e.target.value)} // updates email variable to inputted string
                 required
             />
-            <input className="input2"
+            <br></br>
+
+            <input
                 type="password"
                 placeholder="Password"
                 value={password}
                 onChange={e => setPassword(e.target.value)} // updates password variable to inputted string
                 required
             />
-            <button className="btn1" type="submit">{isLogin ? 'Login' : 'Sign Up'}</button>
+            <br></br>
+
+            {!isLogin && (
+                <>
+                    {role !== 'residency' ? (
+                        <>
+                            <input
+                            type="text"
+                            placeholder="First Name"
+                            value={fname}
+                            onChange={e => setPassword(e.target.value)} // updates firstname variable to inputted string
+                            required
+                            />
+                            <br></br>
+
+                            <input
+                                type="text"
+                                placeholder="Last Name"
+                                value={lname}
+                                onChange={e => setPassword(e.target.value)} // updates lastname variable to inputted string
+                                required
+                            />
+                            <br></br>
+                        </>
+                    ) : (
+                        <>
+                            <input
+                                type="text"
+                                placeholder="Company Name"
+                                value={fname}
+                                onChange={e => setCname(e.target.value)} // updates companyname variable to inputted string
+                                required
+                            />
+                            <br></br>
+                        </>
+                    )}
+
+                    {role === 'student' && (
+                        <>
+                            <input
+                                type="text"
+                                placeholder="Student Id"
+                                value={studentId}
+                                onChange={e => setSID(e.target.value)} // updates student id variable to inputted string
+                                required
+                            />
+                            <br></br>
+                        </>
+                    )}
+                </>
+            )}
+            
+            <button type="submit">{isLogin ? 'Login' : 'Sign Up'}</button>
             <p
                 onClick={() => setIsLogin(!isLogin)}
                 style={{cursor: 'pointer',color: 'blue'}}
