d0c6e994553fea821ac51586c4d6b12086404f5f
CiaranLy <lynchbros3@gmail.com>
2025-05-27 11:25:49 +0100

    bash script to auto run all set and start stuff for front and back

---
+++ epicProject/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'Downloading Frontend Requirments'
+sudo apt update
+sudo apt install -y nodejs npm
+cd jobsBoard/
+npm install
+
+echo 'Downloading Backend Requirments'
+sudo apt install -y python3 python3-pip python3.12-venv
+cd ../backend/
+python3 -m venv venv
+pip install -r requirments.txt
+
+echo 'Starting Backend'
+source venv/bin/activate
+python3 app.py
+
+echo 'Starting Frontend'
+cd ../jobsBoard/
+npm run dev
+
+# run with chmod +x setup.sh for first time and ./setup.sh the rest of the time
+# for windows users run as wsl bash ./setup.sh
