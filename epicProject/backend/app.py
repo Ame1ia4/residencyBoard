f6e0539346231796652c0abadcd7333b3c2dde34
CiaranLy <lynchbros3@gmail.com>
2025-05-27 16:01:48 +0100

    fixed flask url issue

--- epicProject/backend/app.py
+++ epicProject/backend/app.py
@@ -4,9 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__) #creates the flask app and
 CORS(app) #uses CORS on this file so React can talk to it
 
-@app.route('/api/hello') #url on the flask local host that will run the function below
-def hello():
-    return jsonify({'message': 'Hello from flask'})
+@app.route('/') #url on the flask local host that will run the function below
+def test():
+    return 'this homepage'
 
 if __name__ == '__main__': #if this file is run directly then this will run
     app.run(debug=True)
--- epicProject/setup.sh
+++ epicProject/setup.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
 
-echo 'Downloading Frontend Requirments'
+echo 'Downloading Frontend Requirements'
 sudo apt update
 sudo apt install -y nodejs npm
 cd jobsBoard/
 npm install
+cd ..
 
-echo 'Downloading Backend Requirments'
+echo 'Downloading Backend Requirements'
 sudo apt install -y python3 python3-pip python3.12-venv
-cd ../backend/
-python3 -m venv venv
+cd backend/
+
+# Create venv if it doesn't exist
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+
+fuser -k 5000/tcp
+
+# Activate venv and install packages
+source venv/bin/activate
+pip install -r requirements.txt
 
 echo 'Starting Backend'
-source venv/bin/activate
-pip install -r requirments.txt
-python3 app.py
+# Run backend in background
+python3 app.py &
 
 echo 'Starting Frontend'
 cd ../jobsBoard/
 npm run dev
+
 
 # run with chmod +x setup.sh for first time and ./setup.sh the rest of the time
 # for windows users run as wsl bash ./setup.sh
