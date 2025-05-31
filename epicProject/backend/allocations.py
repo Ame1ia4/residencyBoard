31fec0b75dd57bf20d4be04476d4a4222c702e1b
CiaranLy <lynchbros3@gmail.com>
2025-05-31 22:09:22 +0100

    more funny path shenanigans

--- epicProject/backend/allocations.py
+++ epicProject/backend/allocations.py
@@ -3,9 +3,11 @@ from supabase import create_client, Client
 from dotenv import load_dotenv
 import pandas as pd
 
-# creates a supabase client
-load_dotenv(dotenv_path=".env")
-url: str = os.environ.get("VITE_SUPABASE_URL")
-key: str = os.environ.get("VITE_SUPABASE_KEY") 
-print(os.getcwd())
-supabase: Client = create_client(url, key)
+def rpAllocate():
+    # creates a supabase client
+    load_dotenv(dotenv_path=".env")
+    url: str = os.environ.get("VITE_SUPABASE_URL")
+    key: str = os.environ.get("VITE_SUPABASE_KEY") 
+    supabase: Client = create_client(url, key)
+
+    return 'RP'
