975654cdc3828f1f74e7c3cb89f16bb9fe0ddd98
sarahmc253 <sarahmcdon2005@gmail.com>
2025-06-01 18:40:26 +0100

    sorting by year

--- epicProject/backend/loadCSVs.py
+++ epicProject/backend/loadCSVs.py
@@ -4,25 +4,19 @@ from dotenv import load_dotenv
 import pandas as pd
 
 # creates a supabase client
-<<<<<<< HEAD
 load_dotenv(dotenv_path="epicProject/jobsBoard/.env")
 url: str = "https://zahjfkggsyktdshmjmre.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphaGpma2dnc3lrdGRzaG1qbXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5ODEyMzMsImV4cCI6MjA2MzU1NzIzM30.-7nvAbM7nzfHAs3qYwXivZjHMP6dfbX5k3LUByxk09A"
-=======
-load_dotenv(dotenv_path='.env')
-url = os.getenv("VITE_SUPABASE_URL")
-key = os.getenv("VITE_SUPABASE_KEY")
->>>>>>> 31fec0b75dd57bf20d4be04476d4a4222c702e1b
 supabase: Client = create_client(url, key)
 
 # any files downloaded from supabase are stored in the download_directory
 download_directory = "epicProject/backend/csvDownloads"
 os.makedirs(download_directory, exist_ok=True)
 
-#downloads the jobRankings csv from supabase storage and prints it
-global local_path
-local_path = os.path.join(download_directory, "jobRankings.csv")
-with open(local_path, "wb") as f:
+#downloads the jobRankings csv from supabase storage
+global ranking_path
+ranking_path = os.path.join(download_directory, "jobRankings.csv")
+with open(ranking_path, "wb") as f:
     response = (
         supabase.storage
         .from_("ranking-csvs")
@@ -30,13 +24,17 @@ with open(local_path, "wb") as f:
     )
     f.write(response)
 
-# downloads the qca_list csv from supabase storage and prints it
-global qca_path
-qca_path = os.path.join(download_directory, "qca_list.csv")
-with open(qca_path, "wb") as file:
-    response = (
-        supabase.storage
-        .from_("qca-list")
-        .download("qca_list.csv")
-    )
+# downloads the qca_list csv from supabase storage
+def qca_list_download(year_group):
+    filename = f"qca_list_{year_group}.csv"
+
+    qca_path = os.path.join(download_directory, filename)
+    with open(qca_path, "wb") as file:
+        response = (
+            supabase.storage
+            .from_("qca-list")
+            .download("qca_list.csv")
+        )
     file.write(response)
+
+    return qca_path
