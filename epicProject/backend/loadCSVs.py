7d1d59dcaad597ee19f6bc8d267d5673aebf1550
sarahmc253 <sarahmcdon2005@gmail.com>
2025-06-02 18:47:08 +0100

    should be working sort of

--- epicProject/backend/loadCSVs.py
+++ epicProject/backend/loadCSVs.py
@@ -4,9 +4,8 @@ from dotenv import load_dotenv
 import pandas as pd
 
 # creates a supabase client
-load_dotenv(dotenv_path='.env')
-url = os.getenv("VITE_SUPABASE_URL")
-key = os.getenv("VITE_SUPABASE_KEY")
+url = "https://zahjfkggsyktdshmjmre.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphaGpma2dnc3lrdGRzaG1qbXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc5ODEyMzMsImV4cCI6MjA2MzU1NzIzM30.-7nvAbM7nzfHAs3qYwXivZjHMP6dfbX5k3LUByxk09A"
 supabase: Client = create_client(url, key)
 
 # any files downloaded from supabase are stored in the download_directory
@@ -27,14 +26,13 @@ with open(ranking_path, "wb") as f:
 # downloads the qca_list csv from supabase storage
 def qca_list_download(year_group):
     filename = f"qca_list_{year_group}.csv"
-
     qca_path = os.path.join(download_directory, filename)
+    response = (
+        supabase.storage
+        .from_("qca-list")
+        .download(f"qca_list_{year_group}.csv")
+    )
     with open(qca_path, "wb") as file:
-        response = (
-            supabase.storage
-            .from_("qca-list")
-            .download("qca_list.csv")
-        )
-    file.write(response)
+        file.write(response)
 
     return qca_path
